0c8ded3153eb8f0293a604eb20ef14bacf9fc195
ishant162 <ishantrog752@gmail.com>
2023-06-20 13:52:32 +0530

    Experimental Migration
    Signed-off-by: ishant162 <ishantrog752@gmail.com>

--- openfl/experimental/aggregator/__init__.py
+++ openfl/experimental/aggregator/__init__.py
@@ -1,6 +1,10 @@
 # Copyright (C) 2020-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-"""openfl experimental package."""
+
+"""Aggregator package."""
 
 from .aggregator import Aggregator
-from .collaborator import Collaborator
+
+__all__ = [
+    'Aggregator',
+]
